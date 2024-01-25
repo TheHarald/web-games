767f2153003ca350a642565bc7725c86e1d82c5e
Roman Surinov <romasurinov13@mail.ru>
2024-01-26 01:11:22 +0300

    test route

---
+++ src/app/api/route.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    message: "test",
+  });
+}
